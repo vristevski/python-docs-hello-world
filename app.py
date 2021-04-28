8fcfa328340cbce080f37b852a9dfc96a0913e16
Ristevski, Vlade <vlade.ristevski@mmc.com>
2021-04-28 14:04:46 -0400

    testurl

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from VR!"
+    with urllib.request.urlopen('http://www.brainjar.com/java/host/test.html') as response:
+    html = response.read()
+    #print(html)
+    return html
+    #return "Hello, World from VR!"
