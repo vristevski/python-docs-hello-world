a1cbac746aafd82e99997328f22fe0daac44f274
Ristevski, Vlade <vlade.ristevski@mmc.com>
2021-04-28 14:11:06 -0400

    fixed indent duhhh

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     with urllib.request.urlopen('http://www.brainjar.com/java/host/test.html') as response:
-    html = response.read()
+        html = response.read()
     #print(html)
     return html
     #return "Hello, World from VR!"
